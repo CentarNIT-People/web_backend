f088334a376e06e081eef17f2625524fbcf20de6
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-09-17 18:28:10 +0200

    code of conduct and readme

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from deta import Deta
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 deta = Deta("a063wuxg_k8zsfQrTEriaLdJwXbGRrE5DfcQYuaXd")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
