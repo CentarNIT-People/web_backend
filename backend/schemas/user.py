e05962b801c9cea0ffe22c28ca6a7a172dafb08b
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-09-18 17:18:28 +0200

    added more data to the user schema

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -3,9 +3,12 @@ from pydantic import BaseModel
 
 class UserBase(BaseModel):
     username: str
+    password: str
+    full_name: str
     email: str
     age: str
-    password: str
+    status: str
+    avatar: str
 
 
 class User(UserBase):
