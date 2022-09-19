aba29e057c8072e4bc6f374b6f039191a7b61afd
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-09-19 19:25:33 +0200

    Adding Language input functionality with user giving github username instead of entering one skill at a time

--- backend/repo/users.py
+++ backend/repo/users.py
@@ -1,5 +1,7 @@
 from deta import Deta
 from hashlib import sha256
+import httpx
+from collections import Counter
 
 deta = Deta("a063wuxg_k8zsfQrTEriaLdJwXbGRrE5DfcQYuaXd")
 db = deta.Base("users")
@@ -10,13 +12,22 @@ def get_all():
 
 
 def create_user(request):
+    percentages = []
+    languages = [x["language"] for x in httpx.get(
+        f"https://api.github.com/users/{request.github}/repos").json()]
+
+    for name, pct in {x: int(float(y) / len(languages) * 100)
+                      for x, y in Counter(languages).items()}.items():
+        percentages.append({"lang   uage": name, "percentage": pct})
+    request.languages = sorted(
+        percentages, key=lambda d: d['percentage'])[::-1][0:3]
     request.password = sha256(request.password.encode("utf-8")).hexdigest()
     return db.put(dict(request), key=request.username)
 
 
 def find_user(username):
     user = db.get(username)
-    return user if user else 404
+    return user or 404
 
 
 def change_user(request):
--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -5,10 +5,12 @@ class UserBase(BaseModel):
     username: str
     password: str
     full_name: str
+    github: str
     email: str
     age: str
     status: str
     avatar: str
+    languages: list
 
 
 class User(UserBase):
