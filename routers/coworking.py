6d424b68859a6cf188bd34db2c2379feba719b40
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-12-03 16:37:44 +0100

    quick fix with output of main route

--- routers/coworking.py
+++ routers/coworking.py
@@ -1,6 +1,12 @@
 from repo import coworking
+from fastapi import Depends
+from repo.role_checker import RoleChecker
 from fastapi import APIRouter, status, BackgroundTasks
 from schemas.coworking import Table, Floor, UserInTable
+
+
+allowed_roles = RoleChecker(["admin"])
+
 
 router = APIRouter(prefix="/coworking", tags=["Coworking"])
 
@@ -11,7 +17,7 @@ async def get_all():
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_floor(request: Floor, background_tasks: BackgroundTasks):
+async def create_floor(request: Floor, background_tasks: BackgroundTasks, _=Depends(allowed_roles)):
     background_tasks.add_task(coworking.create_floor, request)
     return request
 
@@ -22,14 +28,14 @@ async def get_all_from_floor(floor_number: int):
 
 
 @router.delete("/{floor_number}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_floor(floor_number: int, background_tasks: BackgroundTasks):
+async def delete_floor(floor_number: int, background_tasks: BackgroundTasks, _=Depends(allowed_roles)):
     background_tasks.add_task(coworking.delete_floor, floor_number)
     return {"message": "Floor deleted successfully"}
 
 
 @router.post("/{floor_number}/create_table",
              status_code=status.HTTP_201_CREATED)
-async def create_table(floor_number, request: Table):
+async def create_table(floor_number, request: Table, _=Depends(allowed_roles)):
     return coworking.create_table(floor_number, request)
 
 
@@ -40,13 +46,13 @@ async def get_table(floor_number: int, table_number: int):
 
 @router.put("/{floor_number}/{table_number}",
             status_code=status.HTTP_202_ACCEPTED)
-async def update_table(floor_number: int, request: Table):
+async def update_table(floor_number: int, request: Table, _=Depends(allowed_roles)):
     return coworking.update_table(floor_number, request)
 
 
 @router.delete("/{floor_number}/{table_number}",
                status_code=status.HTTP_202_ACCEPTED)
-async def delete_table(floor_number: int, table_number: int):
+async def delete_table(floor_number: int, table_number: int, _=Depends(allowed_roles)):
     return coworking.delete_table(floor_number, table_number)
 
 
@@ -58,11 +64,11 @@ async def get_people_in_table(floor_number: int, table_number: int):
 
 @router.post("/{floor_number}/{table_number}/{username}",
              status_code=status.HTTP_201_CREATED)
-async def add_person_to_table(request: UserInTable, action: str):
+async def add_person_to_table(request: UserInTable, action: str, _=Depends(allowed_roles)):
     return coworking.add_person_to_table(dict(request), action)
 
 
 @router.delete("/{floor_number}/{table_number}/{username}",
                status_code=status.HTTP_202_ACCEPTED)
-async def remove_person_from_table(request: UserInTable, action: str):
+async def remove_person_from_table(request: UserInTable, action: str, _=Depends(allowed_roles)):
     return coworking.remove_person_from_table(dict(request), action)
